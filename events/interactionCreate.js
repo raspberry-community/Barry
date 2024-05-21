575de919f369a7709aa3ef329ef8d9abeee9ba80
Kos <kbeddek@gmail.com>
2024-05-20 21:54:57 -0500

    Do everything else

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,8 +1,341 @@
-const { Events } = require('discord.js');
+const { forms, modals } = require("./ready.js");
+const {
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+  Events,
+  ActionRowBuilder,
+  PermissionsBitField,
+  AttachmentBuilder,
+} = require("discord.js");
+
+async function handleButtons(interaction) {
+  console.log("Handling button interaction:", interaction.customId);
+
+  if (modals && modals.size !== 0) {
+    const modal = modals.get(interaction.customId.toLowerCase());
+    if (modal) {
+      console.log("Showing modal for interaction:", interaction.customId);
+      await interaction.showModal(modal);
+      return;
+    }
+  }
+
+  if (interaction.customId.toLowerCase().includes("close")) {
+    console.log("Closing ticket for user:", interaction.user.id);
+    const channel = await interaction.client.channels.fetch(
+      interaction.channelId
+    );
+    if (channel) {
+      try {
+        const transcript = await generateTranscript(channel);
+        const transcriptChannelId = forms.find(
+          (form) => form.embed.title === interaction.message.embeds[0].title
+        ).transcriptChannel;
+        const transcriptChannel =
+          interaction.client.channels.cache.get(transcriptChannelId);
+        if (transcriptChannel) {
+          await transcriptChannel.send({
+            content: `Transcript for ticket ${channel} - <@${interaction.channel.topic}>:`,
+            files: [transcript],
+          });
+          console.log("Transcript sent to channel:", transcriptChannel.id);
+          await channel.delete();
+          console.log("Channel deleted:", channel.id);
+        } else {
+          console.log("Transcript channel not found:", transcriptChannelId);
+        }
+      } catch (error) {
+        console.error("Error generating transcript:", error);
+      }
+    }
+  }
+
+  if (interaction.customId.toLowerCase().includes("button")) {
+    for (const form of forms) {
+      const buttons = form.modal.buttons;
+      if (buttons) {
+        const button = buttons.find((button) =>
+          interaction.customId
+            .toLowerCase()
+            .includes(button.customId.toLowerCase())
+        );
+        if (button) {
+          const userId = interaction.customId
+            .replace(button.customId.toLowerCase(), "")
+            .replace("-", "");
+          if (interaction.customId.toLowerCase().includes("open")) {
+            console.log("Opening ticket for user:", userId);
+            const category = interaction.client.channels.cache.get(
+              form.ticketCategory
+            );
+            if (!category) {
+              console.log("Category not found:", form.ticketCategory);
+              return;
+            }
+            try {
+              const channel = await category.guild.channels.create({
+                type: 0,
+                topic: userId,
+                name: `ticket-${userId}`,
+                parent: category,
+                permissionOverwrites: [
+                  {
+                    id: userId,
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                  },
+                  {
+                    id: category.guild.roles.everyone,
+                    deny: [PermissionsBitField.Flags.ViewChannel],
+                  },
+                  {
+                    id: form.rolesToPing[0],
+                    allow: [PermissionsBitField.Flags.ViewChannel],
+                  },
+                  {
+                    id: interaction.client.user.id,
+                    allow: [
+                      PermissionsBitField.Flags.ViewChannel,
+                      PermissionsBitField.Flags.ManageChannels,
+                      PermissionsBitField.Flags.ManageMessages,
+                    ],
+                  },
+                ],
+              });
+              console.log("Channel created:", channel.id);
+              const embed = interaction.message.embeds[0];
+              const actionRow = new ActionRowBuilder();
+              const closeButton = new ButtonBuilder()
+                .setCustomId("close-" + channel.id)
+                .setLabel("Close Ticket")
+                .setStyle(ButtonStyle.Danger);
+              actionRow.addComponents(closeButton);
+              await channel.send({
+                content: `Hello <@${userId}>, your ticket has been opened!`,
+                embeds: [embed],
+                components: [actionRow],
+              });
+              console.log("Ticket message sent to channel:", channel.id);
+              await interaction.message.delete();
+              console.log("Original interaction message deleted");
+            } catch (error) {
+              console.error(
+                "Error creating channel or sending message:",
+                error
+              );
+            }
+          } else if (interaction.customId.toLowerCase().includes("close")) {
+            const channelId = interaction.customId.replace("close-", "");
+            const channel = interaction.client.channels.cache.get(channelId);
+            if (channel) {
+              try {
+                await channel.delete();
+                console.log("Channel deleted:", channelId);
+                await interaction.message.delete();
+                console.log("Original interaction message deleted");
+              } catch (error) {
+                console.error(
+                  "Error deleting channel or interaction message:",
+                  error
+                );
+              }
+            } else {
+              console.log("Channel not found:", channelId);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+async function generateTranscript(channel) {
+  const messages = await channel.messages.fetch({ limit: 100 });
+  const transcript = [];
+
+  messages.reverse().forEach((message) => {
+    const attachments = message.attachments.map((attachment) => attachment.url);
+
+    if (message.embeds.length != 0) {
+      message.embeds.forEach((embed) => {
+        const embedFields = embed.fields.map(
+          (field) => `${field.name}: ${field.value}`
+        );
+        const formattedEmbed = `[${new Date(
+          message.createdTimestamp
+        ).toLocaleString()}] Embed - Title: ${
+          embed.title || "No Title"
+        }, Description: ${
+          embed.description || "No Description"
+        }, Fields: ${embedFields.join(", ")}`;
+        transcript.push(formattedEmbed);
+      });
+    }
+
+    const formattedMessage = `[${new Date(
+      message.createdTimestamp
+    ).toLocaleString()}] ${message.author.tag}: ${message.content}`;
+    transcript.push(formattedMessage);
+    if (attachments.length > 0) {
+      transcript.push(`Attachments: ${attachments.join(", ")}`);
+    }
+  });
+
+  const transcriptContent = transcript.join("\n");
+  const buffer = Buffer.from(transcriptContent, "utf-8");
+  const attachment = new AttachmentBuilder(buffer, {
+    name: `transcript-${channel.id}.txt`,
+  });
+
+  console.log(`Transcript generated for channel: ${channel.id}`);
+  return attachment;
+}
+
+async function checkIfUserHasSubmittedTicket(interaction) {
+  if (forms && forms.length !== 0) {
+    for (const form of forms) {
+      if (form.ticketChannel) {
+        const channel = interaction.client.channels.cache.get(
+          form.ticketChannel
+        );
+        if (channel) {
+          const messages = await channel.messages.fetch();
+          const embeds = messages.filter(
+            (message) => message.embeds.length > 0
+          );
+
+          for (const message of embeds.values()) {
+            const embed = message.embeds[0];
+            if (embed.footer && embed.footer.text === interaction.user.id) {
+              await interaction.reply({
+                content:
+                  "You have already submitted a ticket. Please wait for a response.",
+                ephemeral: true,
+              });
+              console.log(
+                "User has already submitted a ticket:",
+                interaction.user.id
+              );
+              return true;
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
+async function handleModalInput(interaction) {
+  console.log("Handling modal input:", interaction.customId);
+  if (await checkIfUserHasSubmittedTicket(interaction)) return;
+
+  if (forms && forms.length !== 0) {
+    const form = forms.find(
+      (form) =>
+        form.modal &&
+        form.modal.customId.toLowerCase() === interaction.customId.toLowerCase()
+    );
+
+    if (!form) {
+      await interaction.reply({
+        content: "Form configuration not found",
+        ephemeral: true,
+      });
+      console.log("Form configuration not found for:", interaction.customId);
+      return;
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(form.embed.title || "New Submission")
+      .setTimestamp();
+
+    form.modal.inputs.forEach((input) => {
+      const value =
+        "```" + interaction.fields.getTextInputValue(input.customId) + "```";
+      embed.addFields({ name: input.label, value });
+    });
+
+    embed.setColor(form.embed.color || null);
+    embed.setFooter({ text: interaction.user.id });
+    embed.setAuthor({
+      name: interaction.user.tag,
+      iconURL: interaction.user.displayAvatarURL(),
+    });
+
+    const channelId = form.ticketChannel;
+    const channel = interaction.client.channels.cache.get(channelId);
+    let mentionMessage = "";
+
+    for (const role of form.rolesToPing) {
+      mentionMessage += `<@&${role}> `;
+    }
+
+    const actionRow = new ActionRowBuilder();
+    for (const button of form.modal.buttons) {
+      const buttonBuilder = new ButtonBuilder()
+        .setCustomId(button.customId + "-" + interaction.user.id)
+        .setLabel(button.label);
+      switch (button.style) {
+        case "Primary":
+          buttonBuilder.setStyle(ButtonStyle.Primary);
+          break;
+        case "Secondary":
+          buttonBuilder.setStyle(ButtonStyle.Secondary);
+          break;
+        case "Success":
+          buttonBuilder.setStyle(ButtonStyle.Success);
+          break;
+        case "Danger":
+          buttonBuilder.setStyle(ButtonStyle.Danger);
+          break;
+        default:
+          buttonBuilder.setStyle(ButtonStyle.Primary);
+          break;
+      }
+      actionRow.addComponents(buttonBuilder);
+    }
+
+    const options = { embeds: [embed], content: mentionMessage };
+    if (actionRow.components.length > 0) {
+      options.components = [actionRow];
+    }
+
+    if (channel) {
+      await channel.send(options);
+      console.log("Embed and buttons sent to channel:", channel.id);
+    } else {
+      console.log("Channel not found:", channelId);
+    }
+
+    await interaction.reply({
+      content:
+        "Your application has been submitted! Please be patient while we review your application.",
+      ephemeral: true,
+    });
+  }
+}
 
 module.exports = {
-    name: Events.InteractionCreate,
-    execute(interaction, client) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+  name: Events.InteractionCreate,
+  async execute(interaction, client) {
+    console.log(
+      `${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.customId}`
+    );
+
+    try {
+      if (interaction.isButton()) {
+        await handleButtons(interaction);
+      }
+
+      if (
+        interaction.isModalSubmit() &&
+        interaction.customId.toLowerCase().includes("modal")
+      ) {
+        await handleModalInput(interaction);
+      }
+    } catch (error) {
+      console.error("Error handling interaction:", error);
     }
-}
+  },
+};
--- events/ready.js
+++ events/ready.js
@@ -1,62 +1,150 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { EmbedBuilder, Events } = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+  Events,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
 
 function setActivity(readyClient) {
-    readyClient.user.setActivity('https://github.com/raspberry-pie-community', { type: 'WATCHING' });
+  readyClient.user.setActivity("https://github.com/raspberry-pie-community", {
+    type: "WATCHING",
+  });
 }
 
+const modals = new Map();
+const forms = [];
+
 async function loadFormEmbeds(readyClient) {
-    const formPath = path.join(__dirname, '../forms');
-    const formFiles = fs.readdirSync(formPath).filter(file => file.endsWith('.json'));
-    await purgeOldEmbeds(readyClient, formFiles);
-    for (const file of formFiles) {
-        const form = require(path.join(formPath, file));
-        generateFormEmbeds(readyClient, form);
+  const formPath = path.join(__dirname, "../forms");
+  const formFiles = fs
+    .readdirSync(formPath)
+    .filter((file) => file.endsWith(".json"));
+  await purgeOldEmbeds(readyClient, formFiles);
+  for (const file of formFiles) {
+    const form = require(path.join(formPath, file));
+    forms.push(form);
+    if (form.modal) await generateModals(form);
+    await generateFormEmbeds(readyClient, form);
+  }
+}
+
+async function generateModals(form) {
+  if (!form.modal)
+    return console.log(`Form ${form.name} does not have a modal.`);
+  const callerId = form.modal.callerId;
+  const modal = new ModalBuilder();
+  modal.setCustomId(form.modal.customId);
+  modal.setTitle(form.modal.title);
+  const actionRows = [];
+  for (const input of form.modal.inputs) {
+    const actionRow = new ActionRowBuilder();
+    switch (input.type) {
+      case "LongText":
+        actionRow.addComponents(
+          new TextInputBuilder()
+            .setCustomId(input.customId)
+            .setPlaceholder(input.placeholder)
+            .setLabel(input.label)
+            .setStyle(TextInputStyle.Paragraph)
+        );
+        break;
+      case "ShortText":
+        actionRow.addComponents(
+          new TextInputBuilder()
+            .setCustomId(input.customId)
+            .setPlaceholder(input.placeholder)
+            .setLabel(input.label)
+            .setStyle(TextInputStyle.Short)
+        );
+        break;
+      default:
+        console.log(`Unknown input type: ${input.type}`);
+        break;
     }
+    actionRows.push(actionRow);
+  }
+  modal.addComponents(actionRows);
+  modals.set(callerId, modal);
 }
 
 async function generateFormEmbeds(readyClient, form) {
-    if(!form.embed) return console.log(`Form ${form.name} does not have an embed.`);
-    const embed = new EmbedBuilder();
-    if(form.embed.color) embed.setColor(form.embed.color);
-    if(form.embed.title) embed.setTitle(form.embed.title)
-    if(form.embed.description) embed.setDescription(form.embed.description)
-    if(form.embed.footer) embed.setFooter(form.embed.footer)
-    if(form.embed.image) embed.setImage(form.embed.image);
-    if(form.embed.thumbnail) embed.setThumbnail(form.embed.thumbnail);
-    if(form.embed.fields) {
-        for(const field of form.embed.fields) {
-            embed.addField(field.name, field.value, field.inline);
-        }
-    }
+  if (!form.embed)
+    return console.log(`Form ${form.name} does not have an embed.`);
 
-    const postChannel = await readyClient.channels.fetch(form.postChannel).then(channel => channel);
-    console.log(`Posting embed for form ${form.name} in channel ${postChannel}`);
-    postChannel.send({ embeds: [embed] });
+  const embed = new EmbedBuilder()
+    .setColor(form.embed.color || null)
+    .setTitle(form.embed.title || "")
+    .setDescription(form.embed.description || "")
+    .setFooter(form.embed.footer || null)
+    .setImage(form.embed.image || null)
+    .setThumbnail(form.embed.thumbnail || null);
+
+  if (form.embed.fields) {
+    for (const field of form.embed.fields) {
+      embed.addFields({
+        name: field.name,
+        value: field.value,
+        inline: field.inline,
+      });
+    }
+  }
+
+  const actionRow = new ActionRowBuilder();
+  if (form.embed.buttons) {
+    for (const button of form.embed.buttons) {
+      const buttonBuilder = new ButtonBuilder()
+        .setCustomId(button.customId)
+        .setLabel(button.label)
+        .setStyle(
+          ButtonStyle[button.style.toUpperCase()] || ButtonStyle.Primary
+        );
+
+      if (button.url) buttonBuilder.setURL(button.url);
+      actionRow.addComponents(buttonBuilder);
+    }
+  }
+
+  const postChannel = await readyClient.channels.fetch(form.postChannel);
+  console.log(
+    `Posting embed for form ${form.embed.title} in channel ${postChannel.id}`
+  );
+
+  const options = { embeds: [embed] };
+  if (actionRow.components.length > 0) {
+    options.components = [actionRow];
+  }
+
+  await postChannel.send(options);
 }
 
 async function purgeOldEmbeds(readyClient, formFiles) {
-    //Purge embeds sent by the bot
-    for (const file of formFiles) {
-        const form = require(path.join(__dirname, '../forms', file));
-        const postChannel = await readyClient.channels.fetch(form.postChannel).then(channel => channel);
-        const messages = await postChannel.messages.fetch({ limit: 100 });
-        for (const message of messages) {
-            if(message[1].author.id === readyClient.user.id) {
-                message[1].delete();
-            }
-        }
+  for (const file of formFiles) {
+    const form = require(path.join(__dirname, "../forms", file));
+    const postChannel = await readyClient.channels.fetch(form.postChannel);
+    const messages = await postChannel.messages.fetch({ limit: 100 });
+    for (const message of messages.values()) {
+      if (message.author.id === readyClient.user.id) {
+        await message.delete();
+      }
     }
+  }
 }
 
 module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute(readyClient) {
-        console.log('Generating embeds');
-        loadFormEmbeds(readyClient);
-        console.log(`Logged in as ${readyClient.user.tag}`);
-        setActivity(readyClient);
-    }
-}
+  name: Events.ClientReady,
+  once: true,
+  modals,
+  forms,
+  async execute(readyClient) {
+    console.log("Generating embeds");
+    await loadFormEmbeds(readyClient);
+    console.log(`Logged in as ${readyClient.user.tag}`);
+    setActivity(readyClient);
+  },
+};
